b15627dbbbbae51bb7f2a6bd9ff5d5c77ed97df4
ShianWang <984292420@qq.com>
2021-02-09 21:10:30 +0800

    chore: remove unless code

---
+++ test/base.test.ts
@@ -0,0 +1,11 @@
+import memoize from "../src";
+
+function fibonacci (n: number): number {
+  return n <= 1 ? 1 : fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+test('adds 1 + 2 to equal 3', () => {
+  const memoized = memoize<number>(fibonacci)
+  const result = memoized(30)
+  expect(result).toBe(1346269)
+});
