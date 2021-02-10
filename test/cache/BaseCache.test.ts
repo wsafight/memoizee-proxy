35f371e15dc029e76873b59e9162735894f4f6fe
ShianWang <984292420@qq.com>
2021-02-10 17:33:20 +0800

    feat: test change

--- test/cache/BaseCache.test.ts
+++ test/cache/BaseCache.test.ts
@@ -1,35 +1,51 @@
 import BaseCache from "../../src/cache/BaseCache";
 
 describe('BaseCache tests', () => {
-  test('setOrRemove baseCache',() => {
+
+  test('set baseCache',() => {
     const baseCache = new BaseCache()
     expect(baseCache.weak).toBe(false)
+  })
+
+  test('setOrRemove baseCache',() => {
+    const baseCache = new BaseCache()
     baseCache.set('a', 1)
     baseCache.set('b', 1)
     expect(baseCache.cacheMap).toEqual(new Map([['a', 1], ['b', 1]]))
+  })
+
+  test('setOrRemove baseCache',() => {
+    const baseCache = new BaseCache()
+    baseCache.set('a', 1)
+    baseCache.set('b', 1)
     baseCache.clear()
     expect(baseCache.cacheMap).toEqual(new Map())
   })
 
-  test('setOrRemove baseCache',() => {
+
+  test('setWeakMap baseCache',() => {
     const baseCache = new BaseCache(true)
     expect(baseCache.weak).toBe(true)
-    baseCache.set({b: 1}, 12)
-    baseCache.set({b: 1}, 12)
+  })
+
+  test('setWeakMap baseCache',() => {
+    const baseCache = new BaseCache(true)
     expect(baseCache.cacheMap instanceof WeakMap)
       .toBe(true)
+  })
+
+  test('setWeakMap baseCache',() => {
+    const baseCache = new BaseCache(true)
+    baseCache.set({b: 1}, 12)
+    baseCache.set({b: 1}, 12)
     baseCache.clear()
     expect(baseCache.cacheMap).toEqual(new WeakMap())
   })
 
   test('delete baseCache WeakMap',() => {
     const baseCache = new BaseCache(true)
-    expect(baseCache.weak).toBe(true)
     const obj = {}
     baseCache.set(obj, 12)
-    expect(baseCache.has(obj)).toBe(true)
-    baseCache.delete(obj)
-    expect(baseCache.delete(obj)).toBe(false)
-    expect(baseCache.cacheMap).toEqual(new WeakMap())
+    expect(baseCache.delete(obj)).toBe(true)
   })
 })
---
+++ test/getManualFunForCache.test.ts
@@ -0,0 +1,2 @@
+describe('BaseCache tests', () => {
+})
