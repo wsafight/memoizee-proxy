37c96e5316adbb20d20c56bc6ae912bf123c6f65
ShianWang <984292420@qq.com>
2021-02-10 16:36:42 +0800

    feat: add MapOrWeakMap

--- src/utils/getMapOrWeakMapByOption.ts
+++ src/utils/getMapOrWeakMapByOption.ts
@@ -1,3 +1,4 @@
-export default function getMapOrWeakMapByOption(weak?: boolean) {
-  return weak ? new WeakMap() : new Map()
+export default function getMapOrWeakMapByOption<V>(weak?: boolean):
+  Map<string, V> | WeakMap<object, V> {
+  return weak ? new WeakMap<object, V>() : new Map<string, V>()
 }
